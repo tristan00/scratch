8311c1dba4a99afc3f5d6b31979095cb492bdfb5
Tristan Delforge <tristandelforge1@gmail.com>
2019-05-12 13:58:57 -0500

    added lsi draft

---
+++ topic_models/lsi.py
@@ -0,0 +1,40 @@
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+
+
+
+
+class LSI():
+    def __init__(self, num_of_topics = 5):
+        self.num_of_topics = num_of_topics
+
+
+    def fit(self, documents, pca_components = 10):
+        self.tfidf_vec = TfidfVectorizer()
+        self.tfidf_vec.fit(documents)
+        tfidf_out = self.tfidf_vec.transform(documents)
+
+        self.pca = PCA(n_components=pca_components)
+        pca_out = self.pca.fit_transform(tfidf_out)
+
+        self.kmean = KMeans(n_clusters=self.num_of_topics)
+        self.kmean.fit(pca_out)
+
+
+    def predict(self, documents):
+        tfidf_out = self.tfidf_vec.transform(documents)
+        pca_out = self.pca.transform(tfidf_out)
+        return self.kmean.predict(pca_out)
+
+
+
+lsi = LSI(num_of_topics = 5)
+
+
+
+
+
+
+
+
